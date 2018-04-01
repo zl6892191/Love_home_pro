c5a2485cb03241c23549c92afd0f6a7681755fa0
zhuolin <258306908@qq.com>
2018-04-01 18:02:29 +0800

    加载静态文件！

---
+++ love_home/static/js/ihome/register.js
@@ -0,0 +1,81 @@
+function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+}
+
+function generateUUID() {
+    var d = new Date().getTime();
+    if(window.performance && typeof window.performance.now === "function"){
+        d += performance.now(); //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+var uuid = "";
+var last_uuid = '';
+// 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
+function generateImageCode() {
+    // 1.需要生成UUID
+    uuid = generateUUID();
+
+    // last_uuid = uuid;
+
+    // 2.拼接请求地址 ： url = /api/1.0/image_code?uuid=uuid
+    var url = '/api/1.0/image_code?uuid=' + uuid + '&last_uuid=' + last_uuid;
+
+    // 3.将url赋值给<img>标签的src属性
+    // $('.image-code>img') : 表示从image-code标识的标签中直接找到子集<img>
+    // $('.image-code img') : 表示从image-code标识的标签中找子集，如果子集没有，就去子集的子集中找
+    $('.image-code>img').attr('src', url);
+
+    // 当前的uuid使用完成后，立即记录，下次再进入时，之前保存到last_uuid里面就是上次的uuid
+    last_uuid = uuid;
+    
+}
+
+function sendSMSCode() {
+    // 校验参数，保证输入框有数据填写
+    $(".phonecode-a").removeAttr("onclick");
+    var mobile = $("#mobile").val();
+    if (!mobile) {
+        $("#mobile-err span").html("请填写正确的手机号！");
+        $("#mobile-err").show();
+        $(".phonecode-a").attr("onclick", "sendSMSCode();");
+        return;
+    } 
+    var imageCode = $("#imagecode").val();
+    if (!imageCode) {
+        $("#image-code-err span").html("请填写验证码！");
+        $("#image-code-err").show();
+        $(".phonecode-a").attr("onclick", "sendSMSCode();");
+        return;
+    }
+
+    // TODO: 通过ajax方式向后端接口发送请求，让后端发送短信验证码
+}
+
+$(document).ready(function() {
+    generateImageCode();  // 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
+    $("#mobile").focus(function(){
+        $("#mobile-err").hide();
+    });
+    $("#imagecode").focus(function(){
+        $("#image-code-err").hide();
+    });
+    $("#phonecode").focus(function(){
+        $("#phone-code-err").hide();
+    });
+    $("#password").focus(function(){
+        $("#password-err").hide();
+        $("#password2-err").hide();
+    });
+    $("#password2").focus(function(){
+        $("#password2-err").hide();
+    });
+
+    // TODO: 注册的提交(判断参数是否为空)
+})
